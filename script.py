e47e6637129fe60f8d71b2b6c83ce9f9623a4193
ptoshio <144620970+ptoshio@users.noreply.github.com>
2023-09-11 07:03:54 -0300

    Update script.py

--- script.py
+++ script.py
@@ -19,7 +19,15 @@ task_add = []
 #task name, task_id, 4 Célula 5 Doc Quality 8 Paggo Scorer 11 [Dev] Back Points 12 [Dev] Front Points 13 [Paggo] Back Points 14 [Paggo] Front Points
 for task in prodev.projects[1].lists[2].get_all_tasks(include_closed = True):
     if task.id == task_id:
-      task_add = [task.name, task.id, task.assignees, task.custom_fields[4], task.custom_fields[5], task.custom_fields[8], 
-                  task.custom_fields[11], task.custom_fields[12], task.custom_fields[13], task.custom_fields[14]]
+      task_add = {'Nome_Task': task.name, 'ID_Task': task.id, 'Dev' : task.assignees, 'Celula': task.custom_fields[4], 'Qualidade_doc': task.custom_fields[5], 
+                  'Paggo Scorer': task.custom_fields[8], 'Dev Back': task.custom_fields[11], 'Dev Front': task.custom_fields[12], 'Paggo Back': task.custom_fields[13],
+                  'Paggo Front': task.custom_fields[14]}
 
-dataframe.task_add
+excel_file_path = 'data_tasks.xlsx'
+existing_df = pd.read_excel(excel_file_path)
+
+# Append the new row to the existing DataFrame
+updated_df = existing_df.append(task_add, ignore_index=True)
+
+# Save the updated DataFrame back to the Excel file
+updated_df.to_excel(excel_file_path, index=False)
